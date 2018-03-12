1906dc137734ff1293fb3c6f4ac1ed12f3517413
Ops <krzysztof.papciak@hotmail.com>
2018-03-12 16:27:18 +0100

    add tests

---
+++ Weather 2.0Tests/Infrastructure/HttpGrabberTestGrabber.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Weather_2._0.Infrastructure;
+using Weather_2._0.Model;
+using Xunit;
+
+namespace Weather_2._0Tests.Infrastructure
+{
+    public class HttpGrabberTestGrabber
+    {
+        [Fact]
+        public void GetJsonListFromUrlTest()
+        {
+            var data = execute().Result;
+
+          Assert.NotNull(data);
+        }
+        async Task<List<TypeOfData>> execute()
+        {
+            string word = "Rzeszow";
+            var data = await HttpGrabber.GetJsonListFromUrl(word);
+
+            return data;
+        }
+
+        [Fact]
+        public void GetCityFromJsonTest()
+        {
+            var data = HttpGrabber.GetCityFromJson("Opatów");
+            var data2 = HttpGrabber.GetCityFromJson("Opatów");
+            Assert.Equal(data,data2);
+        }
+
+        [Fact]
+        public void Deserialize_JsonWeather()
+        {
+            var city = execute().Result;
+            var weather = city[0].Weather;
+            var result = HttpGrabber.Deserialize_Json(weather).Result;
+            string cityName = result.name;
+            Assert.NotNull(result);
+            Assert.Equal("Rzeszow", cityName);
+        }
+        [Fact]
+        public void Deserialize_JsonWeatherThrow()
+        {
+            var city = execute().Result;
+            var weather = city[0].Weather;
+            var exception = Record.Exception(() => HttpGrabber.Deserialize_Json(weather).Result);   
+            Assert.Null(exception);
+        }
+        [Fact]
+        public void Deserialize_JsonForecast()
+        {
+            var city = execute().Result;
+            var forecast = city[0].Forecast;
+            var result = HttpGrabber.Deserialize_Json(forecast);
+            Assert.NotNull(result);
+        }   
+        [Fact]
+        public void Deserialize_JsonForecastThrow()
+        {
+            var city = execute().Result;
+            var forecast = city[0].Forecast;
+            var exception = Record.Exception(() => HttpGrabber.Deserialize_Json(forecast).Result);
+            Assert.Null(exception);
+        }
+    }
+}
---
+++ Weather 2.0Tests/ViewModel/MainViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using Weather_2._0.ViewModel;
+using  Xunit;
+namespace Weather_2._0Tests.ViewModel
+{
+  public  class MainViewModelTests
+    {
+        private static MainViewModel Execute()
+        {
+            return new MainViewModel();
+        }
+
+        [Fact]
+        public void BeforeInsertValueTest()
+        {
+            Execute().Search = "Rzeszów";
+            var result = Execute().BeforeInsertValue();
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void BeforeInsertValueTest2()
+        {
+            Execute().Search = "Rz";
+            var result = Execute().BeforeInsertValue();
+            Assert.False(result);
+        }
+
+    }
+}
